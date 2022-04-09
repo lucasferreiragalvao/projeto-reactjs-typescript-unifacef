8c2111df2f15fbfce06d915d61968fd0011f258d
Lucas Ferreira Galvão <lucasferreiragalvaoti@gmail.com>
2022-04-09 15:51:21 -0300

    feat: add page cache

--- src/mobx/index.ts
+++ src/mobx/index.ts
@@ -2,6 +2,7 @@ import { home } from '../containers/home/store'
 import { combustivel } from '../containers/combustivel/store';
 import { starWars } from '../containers/star-wars/store';
 import { starWarsDetails } from '../containers/star-wars-details/store';
-import { register} from '../containers/register/store';
+import { register } from '../containers/register/store';
+import { cache } from '../containers/cache/store';
 import { router } from './router.store';
-export { router, home, combustivel, starWars, starWarsDetails, register };
+export { router, home, combustivel, starWars, starWarsDetails, register, cache };
